d3bb67ec79c0e04ed54130a08c91f48dff0f4cad
Katynl <katytun30101981@gmail.com>
2026-05-12 16:52:46 +1000

    Краткое описание того, что вы сделали

--- src/api.js
+++ src/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 const API_BASE = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000/api";
-const api = axios.create({ baseURL: API_BASE });
-
 const api = axios.create({
   baseURL: API_BASE,
 });

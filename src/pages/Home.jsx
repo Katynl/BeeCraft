8b915d13d50b94855c30de0a9e800b923da41eeb
Katynl <katytun30101981@gmail.com>
2026-05-12 18:37:52 +1000

    style h1 home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -46,7 +46,9 @@ const Home = () => {
           <div className="mt-28 mb-28 w-full border-t border-b col-start-1 col-end-3 row-start-1"></div>
           <div className="md:mx-12 border-r border-l h-screen pt-36 px-8 col-start-1 col-end-3 row-start-1">
             <div className="flex w-full justify-between">
-              <h1 className="leading-none font-plexsans ">BLOOM..IING</h1>
+              <h1 className="font-black leading-none tracking-wide font-soledago text-[#f4d864] text-4xl md:text-7xl lg:text-8xl xl:text-9xl">
+                BLOOM..IING
+              </h1>
               <h2 className="text-stone-800">CRAFT</h2>
             </div>
 

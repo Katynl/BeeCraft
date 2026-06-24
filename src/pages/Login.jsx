c645fa1faa75a36e0d489be43302141ca4bae791
Katynl <katytun30101981@gmail.com>
2026-06-24 14:55:54 +1000

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -162,6 +162,7 @@ const Login = () => {
 
       localStorage.setItem("access_token", access);
       localStorage.setItem("refresh_token", refresh);
+      localStorage.setItem("user", JSON.stringify(user));
       localStorage.removeItem("redirectAfterLogin");
 
       navigate(from, { replace: true });
